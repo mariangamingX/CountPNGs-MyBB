0dd5d511a65bc60e7e92f9493e7ce898b8c0d115
mgX <marian.gaming@yandex.com>
2022-06-14 17:15:02 +0300

    images added
    few examples: minecraft, umicons, pricedown(gta font), anime

---
+++ upload/images/counter/um_icons/readme-damnit.txt
@@ -0,0 +1,22 @@
+-------------------------------------------Umicons---------------------------------------------------
+
+Volume 2
+
+Um let's see i've never done a readme before so...
+
+Rules and regulations and all that good stuff concerning these icons:
+These icons are free for personal [PERSONAL!!, I don't want any of you big companies stealing from the little guy thinking no one will notice, I don't want to be the next FOOOD(poor guy
+)] use.
+You can distribute them for free as long as the sets are intact and unmodified, this text file is included with every zip file in the set, and I'm given proper credit and a link to my site. 
+You may not sell them or use them for profit either individually, or in any sort of collection or CD package.
+
+quoted from Brewman's Gorilla readme, I hope he doesn't mind. 
+
+Big shout out to Design Technika² especially Y/C for inspiring me to do these through his using them(he's one of the custo gods, damn it!).  
+
+
+Stealing is wrong etc...
+
+Mattahan.Web1000.com
+
+©Paul Davey 2003. All Rights Reserved.
--- upload/inc/plugins/countpngs.php
+++ upload/inc/plugins/countpngs.php
@@ -28,11 +28,7 @@
 
 if(!defined("IN_MYBB")){
 	die("This file cannot be accessed directly.");
-}
 
-if(!defined("PROSTATS")){
-    define("PROSTATS", MYBB_ROOT."inc/plugins/prostats.php");
-}
 // add hooks
 $plugins->add_hook('admin_load', 'countpngs_admin');
 $plugins->add_hook('admin_tools_menu', 'countpngs_admin_tools_menu');
@@ -54,135 +50,41 @@ function countpngs_info()
 	);
 }
 
-function countpngs_activate()
+
+function countpngs_install()
 {
-	global $db, $lang;
+
+	$template = '<strong>{$counterpngs}</strong>';
+
+	$insert_array = array(
+		'title' => 'counterpngs_template',
+		'template' => $db->escape_string($template),
+		'sid' => '-1',
+		'version' => '',
+		'dateline' => time()
+	);
+	
+	$db->insert_query('templates', $insert_array);
 }
 
+function countpngs_is_installed()
+{
+
+}
+
+function countpngs_uninstall()
+{
+	$db->delete_query("templates", "title = 'counterpngs_template'");
+}
+
+function countpngs_activate()
+{
+
+}
 
 function countpngs_deactivate()
 {
-	global $db, $mybb;
-}
 
-/*************************************************************************************/
-// ADMIN PART
-/*************************************************************************************/
-
-function countpngs_admin_tools_menu(&$sub_menu)
-{
-	global $lang;
-
-	$lang->load('countpngs');
-	$sub_menu[] = array('id' => 'countpngs', 'title' => $lang->countpngs_index, 'link' => 'index.php?module=tools-countpngs');
-}
-
-function countpngs_admin_tools_action_handler(&$actions)
-{
-	$actions['countpngs'] = array('active' => 'countpngs', 'file' => 'countpngs');
-}
-
-function countpngs_admin_permissions(&$admin_permissions)
-{
-  	global $db, $mybb, $lang;
-
-	$lang->load("countpngs", false, true);
-	$admin_permissions['countpngs'] = $lang->countpngs_canmanage;
-
-}
-
-function countpngs_admin()
-{
-	global $db, $lang, $mybb, $page, $run_module, $action_file, $mybbadmin, $plugins;
-
-	$lang->load("countpngs", false, true);
-
-	if($run_module == 'tools' && $action_file == 'countpngs')
-	{
-		$page->add_breadcrumb_item($lang->countpngs, 'index.php?module=tools-countpngs');
-		$page->output_header($lang->countpngs);
-
-		$sub_tabs['countpngs_count'] = array(
-			'title'			=> $lang->countpngs_count,
-			'link'			=> 'index.php?module=tools-countpngs',
-			'description'	=> $lang->countpngs_count_desc
-		);
-
-		$page->output_nav_tabs($sub_tabs, 'countpngs_count');
-
-		if (!$mybb->input['action'])
-		{
-			$form = new Form("index.php?module=tools-countpngs&amp;action=count", "post", "countpngs");
-
-			$form_container = new FormContainer($lang->countpngs_count);
-			$form_container->output_row($lang->countpngs_user, $lang->countpngs_user_desc, $form->generate_text_box('user', '', array('id' => 'user')), 'user');
-			$form_container->output_row($lang->countpngs_exemptforums, $lang->countpngs_exemptforums_desc, $form->generate_text_box('exempt', '', array('id' => 'exempt')), 'exempt');
-			$form_container->output_row($lang->countpngs_date, $lang->countpngs_date_desc, $form->generate_text_box('date', '', array('id' => 'date')), 'date');
-			$form_container->output_row($lang->countpngs_threads, $lang->countpngs_threads_desc, $form->generate_yes_no_radio('countthreads', 0, true, "", ""), 'countthreads');
-
-			$form_container->end();
-
-			$buttons = "";
-			$buttons[] = $form->generate_submit_button($lang->countpngs_submit);
-			$buttons[] = $form->generate_reset_button($lang->countpngs_reset);
-			$form->output_submit_wrapper($buttons);
-			$form->end();
-		}
-		elseif ($mybb->input['action'] == 'count')
-		{
-			$posts = $threads = 0;
-
-			$user = trim($mybb->input['user']);
-			if (!$user || !$db->fetch_field($db->simple_select('users', 'uid', 'username=\''.$db->escape_string($user).'\''), 'uid'))
-			{
-				flash_message($lang->countpngs_invalid_user, 'error');
-				admin_redirect("index.php?module=tools-countpngs");
-			}
-
-			$forums = $mybb->input['exempt'];
-			if ($forums != '')
-			{
-				$forums = explode(',', $forums);
-			}
-			else
-				$forums = array();
-
-			$date = $mybb->input['date'];
-			if (!$mybb->input['date'])
-			{
-				flash_message($lang->countpngs_invalid_date, 'error');
-				admin_redirect("index.php?module=tools-countpngs");
-			}
-			$date = strtotime($date);
-
-			$countthreads = intval($mybb->input['countthreads']);
-
-			$query = $db->simple_select("posts", "pid", "username='".$db->escape_string($user)."' AND dateline > ".$date." AND fid NOT IN ('".implode('\',\'', $forums)."')");
-			while ($post = $db->fetch_array($query))
-			{
-				$posts++;
-			}
-
-			if ($countthreads == 1)
-			{
-				$query = $db->simple_select("threads", "tid", "username='".$db->escape_string($user)."' AND dateline > ".$date." AND fid NOT IN ('".implode('\',\'', $forums)."')");
-				while ($thread = $db->fetch_array($query))
-				{
-					$threads++;
-				}
-
-				flash_message($lang->sprintf($lang->countpngs_total_count_both, $posts, $threads), 'success');
-				admin_redirect("index.php?module=tools-countpngs");
-			}
-			else {
-				flash_message($lang->sprintf($lang->countpngs_total_count, $posts, $threads), 'success');
-				admin_redirect("index.php?module=tools-countpngs");
-			}
-		}
-
-		$page->output_footer();
-		exit;
-	}
 }
 
 ?>
