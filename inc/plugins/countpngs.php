b07150507cc7cfd33b143369809df4287d3bc169
mgX <107469245+mariangamingX@users.noreply.github.com>
2022-06-14 14:10:16 +0300

    Main plugin file

---
+++ inc/plugins/countpngs.php
@@ -0,0 +1,48 @@
+<?php
+
+// Disallow direct access to this file for security reasons
+if(!defined("IN_MYBB"))
+{
+	die("Direct initialization of this file is not allowed.");
+}
+
+
+function countpngs_info()
+{
+	return array(
+		"name"			=> "CountPNGs",
+		"description"	=> "Fully customizable counter based on images for MyBB-based websites.",
+		"website"		=> "",
+		"author"		=> "mgX#2755 on dsc",
+		"authorsite"	=> "https://enigma-t.ml/member.php?action=profile&uid=1",
+		"version"		=> "1.0",
+		"guid" 			=> "",
+		"codename"		=> "countpng",
+		"compatibility" => "*"
+	);
+}
+
+function countpngs_install()
+{
+
+}
+
+function countpngs_is_installed()
+{
+
+}
+
+function countpngs_uninstall()
+{
+
+}
+
+function countpngs_activate()
+{
+
+}
+
+function countpngs_deactivate()
+{
+
+}
