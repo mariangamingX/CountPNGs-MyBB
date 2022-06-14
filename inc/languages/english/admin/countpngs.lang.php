05f4d0a744a0b3cf34e2ba8d1876bf9e6c7ec9fb
mgX <107469245+mariangamingX@users.noreply.github.com>
2022-06-14 14:41:16 +0300

    english language for admincp

---
+++ inc/languages/english/admin/countpngs.lang.php
@@ -0,0 +1,28 @@
+<?php
+/***************************************************************************
+ *
+ *  CountPNGs plugin (/inc/plugins/countpngs.php)
+ *  Author: mgX #2755 on discord
+ *
+ *  License: license.txt
+ *
+ *  Fully customizable counter based on images for MyBB 1.8.
+ *
+ ***************************************************************************/
+
+/****************************************************************************
+                DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+                        Version 2, December 2004
+   
+     Copyright (C) 2022 mgX <marian.gaming@yandex.com>
+     Everyone is permitted to copy and distribute verbatim or modified
+     copies of this license document, and changing it is allowed as long
+     as the name is changed.
+ 
+                DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+       TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+      0. You just DO WHAT THE FUCK YOU WANT TO.
+****************************************************************************/
+
+
+?>
